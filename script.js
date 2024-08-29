853ce678340b7e7495460156fb9136f242a3744c
ALNoman78 <abdullaalno078@gmail.com>
2024-08-29 19:07:15 +0600

    practice task one completed

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+//  if condition true then print this
+
 let  burgerPrice = 599;
 if (burgerPrice > 500){
-    console.log("")
+    console.log(" Burger more than 500tk: free Coke")
+}else{
+    console.log("coke price 39 taka")
+}
+
+
+let  friedRice = 399;
+if (friedRice > 500){
+    console.log(" Burger more than 500tk: free Coke")
+}else{
+    console.log("coke price 39 taka")
 }
