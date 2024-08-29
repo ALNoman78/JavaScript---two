b2c71c8635ed41daee9a69570dc317515bb682f0
ALNoman78 <abdullaalno078@gmail.com>
2024-08-29 19:33:24 +0600

    condition realize and ready for next task

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 //  if condition true then print this
+// practice task - 01
 
-let  burgerPrice = 599;
-if (burgerPrice > 500){
-    console.log(" Burger more than 500tk: free Coke")
+
+// let  burgerPrice = 599;
+// if (burgerPrice > 500){
+//     console.log(" Burger more than 500tk: free Coke")
+// }else{
+//     console.log("coke price 39 taka")
+// }
+
+
+// let  friedRice = 399;
+// if (friedRice > 500){
+//     console.log(" Burger more than 500tk: free Coke")
+// }else{
+//     console.log("coke price 39 taka")
+// }
+
+
+// complex condition
+
+let salary = 32000;
+let isBCS = true;
+let height = 66;
+
+if((salary > 380000 && height > 64) || isBCS == false){
+    console.log("You are eligible to marry")
+}else if(salary > 30000 && (isBCS == false || height < 85)){
+    console.log("You are ok to marry")
 }else{
-    console.log("coke price 39 taka")
-}
-
-
-let  friedRice = 399;
-if (friedRice > 500){
-    console.log(" Burger more than 500tk: free Coke")
-}else{
-    console.log("coke price 39 taka")
+    console.log("You aren't eligible to married n")
 }
