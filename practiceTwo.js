61ad9d4e78433ebd38ee4849918ed6d1cc78d9da
ALNoman78 <abdullaalno078@gmail.com>
2024-08-30 18:21:18 +0600

    temperature file added

--- practiceTwo.js
+++ practiceTwo.js
@@ -1,16 +1,19 @@
+// - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
 // BMI < 18.5, you are underweight.
 //     - BMI >= 18.5 and BMI <=24.9, you are normal.
 //     - BMI >=25 and BMI <= 29.9, you are overweight.
 //     - Otherwise, you are obese.
 
-let weightBmi = 43;
+let weight = 68;
+let height = 1.72;
+let bmi = weight / (height ** 2);
 
-    if (weightBmi < 18.4) {
-    console.log("You are under weight");
-    } else if (weightBmi >= 18.5 && weightBmi <= 24.9) {
-    console.log("You are Normal");
-    } else if (weightBmi >= 25 && weightBmi <= 29.9) {
-    console.log("You are over weight");
-    } else {
-    console.log(" You are obese.");
-    }
+if (bmi < 18.4) {
+  console.log("You are under weight");
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+  console.log("You are Normal");
+} else if (bmi >= 25 && bmi <= 29.9) {
+  console.log("You are over weight");
+} else {
+  console.log(" You are obese.");
+}
---
+++ temp.js
@@ -0,0 +1,8 @@
+let temp = 23;
+ if ( temp >= 30){
+    console.log(temp, "Temperature feels hot")
+ }else if( temp >= 15 && temp <= 25){
+    console.log(temp , "It feels cold")
+ }else{
+    console.log("If feels cold outside")
+ }
